66e8b90cdbe713b2fed18d39f31c10450830bf8e
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-04 16:25:56 -0500

    Create example node.js server

---
+++ exampleFiles/exampleServer.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  let method = req.method;
+  let url = req.url;
+  
+  console.log(`${method} | ${url}`)
+  if(url === "/") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end('<h1>Hello World</h1>');   
+  } else {
+    res.writeHead(404, {"Content-Type": "text/html"});
+    res.end('<h1 style="color:red">Page not found</h1>');
+  }
+});
+
+
+server.listen(3000, () => console.log(`Go to http://localhost:${PORT}`));
