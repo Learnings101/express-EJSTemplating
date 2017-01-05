27d1727daa4d394ac96fa00b81d094b929a08d0c
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-05 13:30:20 -0500

    Add data for home route to use with EJS

--- exampleFiles/templatingExample/server.js
+++ exampleFiles/templatingExample/server.js
@@ -6,7 +6,10 @@ app.set('views', __dirname + '/views') //specify the views directory
 app.set('view engine', 'ejs') //register the template engine
 
 app.get('/', (req, res) => {
-  res.render('default.ejs');
+  res.render('default.ejs', {
+    title: 'Homepage', 
+    users: ['Tom', 'Dick', 'Harry']
+  });
 });
 
 app.listen(PORT, () => console.log(`Visit http://localhost:${PORT}`));
