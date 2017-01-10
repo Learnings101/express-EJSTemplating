d0f61973865f7ae6047ff9ffbdfa7be3a2346031
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-09 22:13:51 -0800

    Add route for 'about us'

--- exampleFiles/templatingExample/server.js
+++ exampleFiles/templatingExample/server.js
@@ -7,10 +7,20 @@ app.set('view engine', 'ejs') //register the template engine
 
 app.get('/', (req, res) => {
   res.render('default.ejs', {
-    title: 'Homepage', 
+    title: 'Homepage',
+    classname: 'home',
     users: ['Tom', 'Dick', 'Harry']
   });
 });
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs', {
+    title: 'About Us',
+    classname: 'about'
+  });
+});
+
+app.get('*', (req, res) => res.send('Bad Route'));
 
 app.listen(PORT, () => console.log(`Visit http://localhost:${PORT}`));
 
